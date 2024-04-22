31aa5195052b3640a4c85ac21f8020e05f5e32dd
sgatc <sgatchalian105@gmail.com>
2024-04-22 17:38:56 +0800

    changes to RewardValueTests.java 1) changes to convert_from_cash_to_miles()   - added functionality to test cash to miles conversion 2) changes to convert_from_miles_to_cash()   - added functionality to test miles to cash conversion

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        int milesValue = 10000;
+        double cashValue = milesValue / 0.0035;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(cashValue, rewardValue.getCashValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        double cashValue = 100.0;
+        double milesValue = cashValue * 0.0035;
+        var rewardValue = new RewardValue(cashValue);
+        assertEquals(milesValue, rewardValue.getMilesValue());
     }
 }
